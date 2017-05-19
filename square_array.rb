a0c609ce64fd7513e17df2990bb0fc77e7ce6f84
davidtom <davidjtomczyk@gmail.com>
2017-05-19 14:40:38 -0400

    completed lab

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,15 @@
 def square_array(array)
-  # your code here
+  new_array = []
+
+  array.each do |item|
+    new_array.push(item ** 2)
+  end
+
+  new_array
+end
+
+def square_array_collect(array)
+
+  array.collect {|item| item ** 2}
+
 end
